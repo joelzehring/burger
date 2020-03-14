51c7d4e3af8642952a0cfb670d2816a13e570d4f
Joel Zehring <joelzehring@gmail.com>
2020-03-14 09:37:36 -0700

    more server.js debugging

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 
+/*
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -27,3 +28,4 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+*/
